7ec22754ca273608e73227d5bbea1635efa372c2
unknown <hautersolomon@gmail.com>
2024-09-14 09:12:21 -0500

    a

--- getajoke/settings.py
+++ getajoke/settings.py
@@ -44,7 +44,7 @@ LOGGING = {
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with daebug turned on in production!
 DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
